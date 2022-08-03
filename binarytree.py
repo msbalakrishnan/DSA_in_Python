c34021ea4b63d3ff906661acd347c7e7e952b117
Balakrishnan M <84440964+msbalakrishnan@users.noreply.github.com>
2022-08-03 10:08:26 +0100

    Create binarytree.py

---
+++ binarytree.py
@@ -0,0 +1,42 @@
+class BinaryTree:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = BinaryTree(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = BinaryTree(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+
+for i in range(int(input("enter how many number you want : "))):
+     if i ==0:
+        root = BinaryTree(int(input("enter the root element :")))
+     else:
+        root.insert(int(input(" enter the element: ")))
+# root.insert(55)
+# root.insert(60)
+# root.insert(20)
+# root.insert(52)
+
+root.PrintTree()
